f3d1a25b36f1e5c8fef38669019f7ec0ba944330
shamil <for.photos.only001@gmail.com>
2026-01-22 13:19:35 +0530

    rich text editor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import CreatePost from "./pages/CreatePost"
+
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <CreatePost/>
     </>
   )
 }
