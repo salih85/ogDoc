76d9c6355dd6402309166ce11d980e5b2e97c4d2
shamil <for.photos.only001@gmail.com>
2026-02-02 10:35:57 +0530

    added: library rendering url

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(helmet())
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175",process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175",process.env.FRONTEND_URL,process.env.LIBRARY_URL],
     credentials:true
   })
 );
